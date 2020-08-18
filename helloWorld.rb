fa623fec1b1c65504ee8506879c856cbaef0c285
robert-cummins <robert.k.cummins@gmail.com>
2020-08-19 09:29:32 +1200

    hello world demonstration

---
+++ helloWorld.rb
@@ -0,0 +1,12 @@
+print "Hello World"
+puts "Hello World"
+p "Hello World"
+
+greetings = "Hello World"
+puts greetings
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello(greetings)
