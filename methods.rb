fda4858bb1700fe17ff02404a17ca3deaaa32bcd
robert-cummins <robert.k.cummins@gmail.com>
2020-08-19 21:56:18 +1200

    method examples

---
+++ methods.rb
@@ -0,0 +1,20 @@
+def multiply(first_num, second_num)
+    puts first_num.to_f * second_num.to_f
+end
+
+def addition(first_num, second_num)
+    puts first_num.to_f + second_num.to_f
+end
+
+def subtract(first_num, second_num)
+    puts first_num.to_f - second_num.to_f
+end
+
+def division(first_num, second_num)
+    puts first_num.to_f / second_num.to_f
+end
+
+multiply(3, 5)
+addition(2, 6)
+subtract(8, 2)
+division(10, 2)
