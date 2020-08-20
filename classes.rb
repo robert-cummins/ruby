16004870ee58ec6ff8a0ce0a1338a02b6dfd1964
robert-cummins <robert.k.cummins@gmail.com>
2020-08-21 09:26:36 +1200

    change object by interacting with each other

--- classes.rb
+++ classes.rb
@@ -1,15 +1,23 @@
 class Student
 	attr_accessor :first_name, :last_name, :email, :username, :password
 
-	def initialize(first_name, last_name, username, email, password)
+	def initialize(first_name, last_name, email, username, password)
 		@first_name = first_name
 		@last_name = last_name
 		@email = email
 		@username = username
 		@password = password
 	end
+
+	def to_s
+		"First name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username: #{@username}"
+	end
 end
 
 robert = Student.new("Robert", "Cummins", "edkjsh@gmail.com", "robert233", "1234")
-john = Student.new("John", "Cummins", "edksdfsdjsh@gmail.com", "john23ds3", "12sadf34")
+john = Student.new("John", "Sull", "edksdfsdjsh@gmail.com", "john23ds3", "12sadf34")
+puts robert
+puts john
+
+robert.last_name = john.last_name
 puts robert
