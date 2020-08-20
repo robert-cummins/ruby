2235f285b6b74dcdd42832cac4c507a6dc608b51
robert-cummins <robert.k.cummins@gmail.com>
2020-08-21 11:03:43 +1200

    create student class

---
+++ project3/student.rb
@@ -0,0 +1,18 @@
+class Student
+	attr_accessor :first_name, :last_name, :email, :username, :password
+
+	def initialize(first_name, last_name, email, username, password)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@username = username
+		@password = password
+	end
+
+	def to_s
+		"First name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username: #{@username}"
+	end
+end
+
+robert = Student.new("Robert", "Cummins", "edkjsh@gmail.com", "robert233", "1234")
+john = Student.new("John", "Sull", "edksdfsdjsh@gmail.com", "john23ds3", "12sadf34")
